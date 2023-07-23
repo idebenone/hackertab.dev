e51d0d30d77020ce5b55e0db1698a9504aa42fa8
John Doe <elmehdi.sakout@gmail.com>
2023-07-23 18:21:50 +0100

    remove migration code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, useEffect, useLayoutEffect, useState } from 'react'
+import { useEffect, useLayoutEffect, useState } from 'react'
 import 'react-contexify/dist/ReactContexify.css'
 import 'src/assets/App.css'
 import { DNDLayout, Header } from 'src/components/Layout'
@@ -6,14 +6,10 @@ import { MarketingBanner } from 'src/features/MarketingBanner'
 import { BookmarksSidebar } from 'src/features/bookmarks'
 import { setupAnalytics, setupIdentification, trackPageView } from 'src/lib/analytics'
 import { useUserPreferences } from 'src/stores/preferences'
-import { diffBetweenTwoDatesInDays } from 'src/utils/DateUtils'
 import { AppContentLayout } from './components/Layout'
 import { isWebOrExtensionVersion } from './utils/Environment'
-import { getAppVersion } from './utils/Os'
-
-const OnboardingModal = React.lazy(() =>
-  import('src/features/onboarding').then((module) => ({ default: module.OnboardingModal }))
-)
+import { lazyImport } from './utils/lazyImport'
+const { OnboardingModal } = lazyImport(() => import('src/features/onboarding'), 'OnboardingModal')
 
 const intersectionCallback = (entries: IntersectionObserverEntry[]) => {
   entries.forEach((entry) => {
@@ -29,26 +25,8 @@ export const App = () => {
   const [showSideBar, setShowSideBar] = useState(false)
   const [showSettings, setShowSettings] = useState(false)
   const [showOnboarding, setShowOnboarding] = useState(true)
-  const {
-    onboardingCompleted,
-    firstSeenDate,
-    markOnboardingAsCompleted,
-    maxVisibleCards,
-    isDNDModeActive,
-    DNDDuration,
-    setDNDDuration,
-  } = useUserPreferences()
-
-  useLayoutEffect(() => {
-    const appVersion = getAppVersion()
-    if (!onboardingCompleted && appVersion && appVersion <= '1.15.9') {
-      const usageDays = diffBetweenTwoDatesInDays(firstSeenDate, Date.now())
-      if (usageDays > 0) {
-        markOnboardingAsCompleted(null)
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [onboardingCompleted, firstSeenDate])
+  const { onboardingCompleted, maxVisibleCards, isDNDModeActive, DNDDuration, setDNDDuration } =
+    useUserPreferences()
 
   useLayoutEffect(() => {
     document.documentElement.style.setProperty('--max-visible-cards', maxVisibleCards.toString())
@@ -90,12 +68,7 @@ export const App = () => {
 
       <div className="App">
         {!onboardingCompleted && isWebOrExtensionVersion() === 'extension' && (
-          <Suspense fallback={null}>
-            <OnboardingModal
-              showOnboarding={showOnboarding}
-              setShowOnboarding={setShowOnboarding}
-            />
-          </Suspense>
+          <OnboardingModal showOnboarding={showOnboarding} setShowOnboarding={setShowOnboarding} />
         )}
         <Header
           setShowSideBar={setShowSideBar}
