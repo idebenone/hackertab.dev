ade4165033cd29a2433dd3690f0f21811dee1438
John Doe <elmehdi.sakout@gmail.com>
2023-07-23 18:52:43 +0100

    remove headers

--- src/lib/interceptors/DefaultRequestInterceptor.ts
+++ src/lib/interceptors/DefaultRequestInterceptor.ts
@@ -1,16 +1,12 @@
 import { AxiosRequestConfig } from 'axios'
 import { API_ENDPOINT } from 'src/config'
-import { getBrowserName, isProduction, isWebOrExtensionVersion } from 'src/utils/Environment'
-import { getAppVersion } from 'src/utils/Os'
+import { isProduction } from 'src/utils/Environment'
 
 export function DefaultRequestInterceptor(config: AxiosRequestConfig) {
   if (config) {
     config.baseURL = isProduction() ? API_ENDPOINT : '/api'
     if (config.headers) {
       config.headers.Accept = 'application/json'
-      config.headers['App-Version'] = getAppVersion() || '0.0.0'
-      config.headers['App-Platform'] = isWebOrExtensionVersion()
-      config.headers['Browser-Name'] = getBrowserName()
     }
   }
 
